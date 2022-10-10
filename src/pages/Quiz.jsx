67d714f1c899bfb8a105db3426e0157fada096c8
Arif5577 <arifalamm33@gmail.com>
2022-10-11 01:09:44 +0600

    handle correct and incorrect answer

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Quiz = () => {
   const { name, id, questions } = useLoaderData();
 
   console.log(useLoaderData());
+  const [correctAnswer, setCorrectAnswer] = useState(0);
+  const [InCorrectAnswer, setInCorrectAnswer] = useState(0);
+
+  const handleCorrectAnswer = () => {
+    setCorrectAnswer((prev) => prev + 1);
+    alert("Correct answer");
+  };
+  const handleInCorrectAnswer = () => {
+    setInCorrectAnswer((prev) => prev + 1);
+    alert("InCorrect answer");
+  };
 
   return (
     <section>
@@ -12,8 +23,14 @@ const Quiz = () => {
         <h1 className="text-2xl uppercase  font-bold text-orange-500 tracking-wider">
           {name}
         </h1>
-        <p className="text-gray-400 mt-3">Correct Answer : 0</p>
-        <p className="text-gray-400">Incorrect Answer : 0</p>
+        <p className="text-gray-400 mt-3">
+          Correct Answer :{" "}
+          <span className="text-slate-900 font-bold">{correctAnswer}</span>
+        </p>
+        <p className="text-gray-400">
+          Incorrect Answer :{" "}
+          <span className="text-red-500 font-bold">{InCorrectAnswer}</span>
+        </p>
       </div>
       <div className="lg:w-1/2 w-full mx-auto my-5">
         {questions?.map((qt) => (
@@ -21,7 +38,17 @@ const Quiz = () => {
             <h2>{qt.question}</h2>
             <ul className="flex flex-col gap-3 mt-4">
               {qt.options.map((option) => (
-                <li key={option}>{option}</li>
+                <li
+                  className="cursor-pointer hover:tracking-wider duration-300 transition-all hover:text-orange-500"
+                  key={option}
+                  onClick={() =>
+                    qt.correctAnswer === option
+                      ? handleCorrectAnswer()
+                      : handleInCorrectAnswer()
+                  }
+                >
+                  {option}
+                </li>
               ))}
             </ul>
           </div>

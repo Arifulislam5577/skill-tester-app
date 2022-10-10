7da23c0db1869972b77752a2d599acec2aa89957
Arif5577 <arifalamm33@gmail.com>
2022-10-10 23:03:37 +0600

    react router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import react from "react";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Statistics from "./pages/Statistics";
 function App() {
-  return <h1>Assignemnt 09</h1>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/quiz/:id",
+          element: <Quiz />,
+        },
+        {
+          path: "/statistics",
+          element: <Statistics />,
+        },
+        {
+          path: "/blog",
+          element: <Blog />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;

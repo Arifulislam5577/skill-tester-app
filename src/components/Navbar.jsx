499e3509d88ec048c431b45e36cd6aa5f0d2648c
Arif5577 <arifalamm33@gmail.com>
2022-10-10 23:10:52 +0600

    Header Setup done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="p-5 bg-slate-900 text-white">
-      <h1>Navbar</h1>
-    </div>
+    <nav className="py-6 bg-slate-900 text-white">
+      <div className="container flex items-center justify-between">
+        <div className="logo">
+          <Link to="/" className="font-bold uppercase">
+            Skill <span className="text-orange-500">Tester</span>
+          </Link>
+        </div>
+
+        <ul className="flex items-center justify-between gap-5">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "text-orange-500" : "text-white"
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/statistics"
+              className={({ isActive }) =>
+                isActive ? "text-orange-500" : "text-white"
+              }
+            >
+              Statistics
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) =>
+                isActive ? "text-orange-500" : "text-white"
+              }
+            >
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 

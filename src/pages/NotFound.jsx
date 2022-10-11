0ace9debe541aba320c0440878f0888143bd2e40
Arif5577 <arifalamm33@gmail.com>
2022-10-11 12:20:39 +0600

    readme file live link added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <section className="h-[80vh] flex flex-col items-center justify-center">
-      <h1 className="lg:text-8xl tracking-widest text-2xl font-bold text-red-500">
+      <h1 className="lg:text-8xl tracking-widest text-2xl font-bold text-red-500 animate-bounce">
         404
       </h1>
       <h1 className="lg:text-4xl text-2xl font-bold text-gray-400 mt-2 uppercase">
